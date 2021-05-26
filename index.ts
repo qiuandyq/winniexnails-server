3df96f03eff8dc71e84d907df36b3ea9e960ce47
unknown <kevinqiu96@gmail.com>
2021-05-26 13:32:51 -0700

    a

--- index.ts
+++ index.ts
@@ -1,10 +1,6 @@
 import express from 'express';
 
+const PORT = process.env.PORT || 3000;
 const app = express();
-const PORT = 8000;
-app.get('/', (req,res) => res.send(
-  'Express + TypeScript Server x')
-);
-app.listen(PORT, () => {
-  console.log(`⚡️[server]: Server is running at localhost:${PORT}`);
-});
+app.get('/', (req, res) => res.send('Hello World!'));
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
